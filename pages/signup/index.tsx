688dc7110292178c72f341da311a7bd31ae7e82f
unknown <vradriano@hotmail.com>
2022-11-17 07:13:57 -0300

    Add axios and create baseurl, handling create user

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,8 +1,40 @@
-import { Button, Container, Box, Paper, FormControl, Input, InputLabel, Typography } from "@mui/material"
+import {
+  Button,
+  Container,
+  Box,
+  Paper,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography
+} from "@mui/material"
+import { api } from "../../src/services/axios"
 import { Header } from "../../src/Components/Header"
 import { styles } from "./styles"
+import { SyntheticEvent, useState } from "react"
 
 const SignUp = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleChangeUsername(username: string) {
+    setUsername(username)
+  }
+
+  function handleChangePassword(password: string) {
+    setPassword(password)
+  }
+
+  async function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault()
+
+    await api.post('/users/create', {
+      username,
+      password
+    })
+
+  }
+
   return (
     <Box>
       <Header />
@@ -18,44 +50,46 @@ const SignUp = () => {
           >
             Cadastre-se
           </Typography>
+          
+          <form onSubmit={handleSubmit}>          
+            <FormControl fullWidth>
+              <InputLabel
+                sx={styles.inputText}
+              >
+                Username
+              </InputLabel>
+              <Input
+                name="username"
+                type="text"
+                onChange={(e) => handleChangeUsername(e.target.value)}
+                sx={styles.inputStyles}
+              />
+            </FormControl>
 
-          <FormControl fullWidth>
-            <InputLabel
-              sx={styles.inputText}
+            <FormControl fullWidth sx={{ mt: 4 }}>
+              <InputLabel
+                sx={styles.inputText}
+              >
+                Senha
+              </InputLabel>
+              <Input
+                name="password"
+                type="password"
+                onChange={(e) => handleChangePassword(e.target.value)}
+                sx={styles.inputStyles}
+              />
+            </FormControl>
+
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={styles.buttonStyles}
             >
-              Username
-            </InputLabel>
-            <Input
-              name="username"
-              type="text"
-              onChange={() => {}}
-              sx={styles.inputStyles}
-            />
-          </FormControl>
-
-          <FormControl fullWidth sx={{ mt: 4 }}>
-            <InputLabel
-              sx={styles.inputText}
-            >
-              Senha
-            </InputLabel>
-            <Input
-              name="password"
-              type="password"
-              onChange={() => {}}
-              sx={styles.inputStyles}
-            />
-          </FormControl>
-
-          <Button
-            fullWidth
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={styles.buttonStyles}
-          >
-            Cadastrar
-          </Button>
+              Cadastrar
+            </Button>
+          </form>
         </Paper>
       </Container>
     </Box>
